b90b06fe6f01b3e4299062790e7c41f09f6a49e9
Grace Anspach <123026604+Ganspach01@users.noreply.github.com>
2023-02-22 16:00:28 -0500

    Add files via upload

---
+++ Mid12.py
@@ -0,0 +1,6 @@
+#Grace Anspach
+def if_divisible_five(x):
+    if x%5==0:
+        print("Yes")
+    else:
+        print("No")
---
+++ Mid14.py
@@ -0,0 +1,4 @@
+#Grace Anspach
+def next_stop(location):
+    print("Next stop is", location)
+    print("Next stop is", location)
